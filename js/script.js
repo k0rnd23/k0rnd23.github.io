4e1289b46d23eac5029419a3aeb48ffbc4aeba94
k0rnd23 <122616170+k0rnd23@users.noreply.github.com>
2023-07-25 04:13:20 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -88,9 +88,6 @@ document.addEventListener("DOMContentLoaded", function () {
     "media/video1.mp4",
     "media/video2.mp4",
     "media/video3.mp4",
-    "media/video4.mp4",
-    "media/video5.mp4",
-    "media/video6.mp4",
   ];
 
   const randomIndex = Math.floor(Math.random() * videoFiles.length);
